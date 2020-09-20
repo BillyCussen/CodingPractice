e59f194017f0e82db0e7bd8d19defb601c3e4aa9
BillyCussen <billycussen0@hotmail.com>
2020-09-20 18:51:30 +0100

    Adding new Files

---
+++ Software Development Module/Project/Lottery.java
@@ -0,0 +1,15 @@
+/*
+Lottery.java
+Billy Cussen
+20/09/2020
+*/
+
+//In Progress
+
+public class Lottery{
+
+    public static void main(String [] args){
+        
+    }
+
+}
---
+++ Software Development Module/Project/LotteryApp.java
@@ -0,0 +1,15 @@
+/*
+LotteryApp.java
+Billy Cussen
+20/09/2020
+*/
+
+//In Progress
+
+public class LotteryApp{
+
+    public static void main(String [] args){
+
+    }
+
+}
