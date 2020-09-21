c425a4d2ee00b2021a0e8083dea56d41e3acde79
BillyCussen <billycussen0@hotmail.com>
2020-09-21 19:20:14 +0100

    Added Input fields to App class and Random Number method to Instantiable class

--- Software Development Module/Project/Lottery.java
+++ Software Development Module/Project/Lottery.java
@@ -6,10 +6,29 @@ Billy Cussen
 
 //In Progress
 
+import java.util.HashSet;
+
 public class Lottery{
 
-    public static void main(String [] args){
-        
+    private int[] randomNumbers;
+    private final int LENGTH = 6;
+
+    //Constructor
+    public Lottery(){
+        this.randomNumbers = new int[this.LENGTH];
+    }
+
+    public int[] generateRandomNumbers(){
+        HashSet<Integer> randomNumbersTemp = new HashSet<Integer>();
+        for(int i = 0; i < this.LENGTH; i++){
+            int randomNumber = (int) (Math.random()*40)+1;
+            while(randomNumbersTemp.contains(randomNumber)){
+                randomNumber = (int) (Math.random()*40)+1;
+            }
+            randomNumbersTemp.add(randomNumber);
+            this.randomNumbers[i] = randomNumber;
+        }
+        return this.randomNumbers;
     }
 
 }
--- Software Development Module/Project/LotteryApp.java
+++ Software Development Module/Project/LotteryApp.java
@@ -6,9 +6,82 @@ Billy Cussen
 
 //In Progress
 
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class LotteryApp{
 
     public static void main(String [] args){
+
+        //Variables
+        boolean inputCheck;
+        int linesToBePlayed = 0;
+        final int LENGTH = 6;
+        int[][] playerNumbers;
+
+        //Objects
+        Scanner keyboard;
+        Lottery myLotteryApp = new Lottery();
+        
+        //Description
+        System.out.println("***LOTTERY GAME***");
+        System.out.println("Description: 6 random secret lottery numbers between 1 and 40 are generated!");
+        System.out.println("Rule 1: You can play up to 3 lines per game!");
+        System.out.println("Rule 2: You can guess 6 Numbers per line!");
+        System.out.println("Rule 3: If you guess 3 or more numbers, you'll win a prize!\n");
+
+        //Input
+        do{
+            keyboard = new Scanner(System.in);
+            inputCheck = true;
+
+            try{
+                System.out.print("Please enter Number of lines you wish to play (1, 2 or 3): ");
+                linesToBePlayed = keyboard.nextInt();
+
+                if(linesToBePlayed<1||linesToBePlayed>3){
+                    throw new InputMismatchException();
+                }
+
+            }catch(InputMismatchException | NumberFormatException e){
+                System.out.println("You need to enter either 1, 2 or 3!");
+                inputCheck = false;
+            }finally{
+                keyboard = null;
+            }
+
+        }while(!inputCheck);
+
+        playerNumbers = new int[linesToBePlayed][LENGTH];
+
+        for(int i = 0; i < linesToBePlayed; i++){
+                do{
+                    inputCheck = true;
+                    keyboard = new Scanner(System.in);
+                    String userNumbersInput = new String();
+                    
+                    try{
+                        System.out.print("Please enter your 6 numbers for line "+(i+1)+": ");
+                        userNumbersInput = keyboard.nextLine();
+                        String[] userNumbersTemp = userNumbersInput.split(" ");
+
+                        if(userNumbersTemp.length != 6){
+                            throw new InputMismatchException();
+                        }
+                        
+                        for(int j = 0; j < userNumbersTemp.length; j++) {
+                            playerNumbers[i][j] = Integer.parseInt(userNumbersTemp[j]);
+                        }
+
+                    }catch(InputMismatchException | NumberFormatException e){
+                        System.out.println("You need to enter 6 Integers between 1 and 40 e.g. \"1 2 39 2 1 27\"");
+                        inputCheck = false;
+                    } finally{
+                        keyboard = null;
+                    }
+
+                }while(!inputCheck);
+        }
 
     }
 
