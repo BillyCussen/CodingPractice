fbd1192eeeb3023c041cfdfc07dd604021545700
BillyCussen <billycussen0@hotmail.com>
2020-11-18 20:56:14 +0000

    Refactored Code for Palindrome exercise as logic was incorrect

--- Java/Data Structures & Algorithms Module - Java/Week8/Revision/Week7/Palindrome.java
+++ Java/Data Structures & Algorithms Module - Java/Week8/Revision/Week7/Palindrome.java
@@ -8,12 +8,12 @@ public class Palindrome{
     public static void main(String[]args){
 
         long startTime = System.nanoTime();
-        System.out.println("Palindrome: "+palindrome("moddom"));
+        System.out.println("Palindrome: "+palindrome("racecar"));
         long endTime = System.nanoTime();
         System.out.println("Looped Approach took "+(endTime-startTime)+" nanoseconds");
         
         startTime = System.nanoTime();
-        System.out.println("Palindrome Recursion: "+palindromeRecursion("moddom"));
+        System.out.println("Palindrome Recursion: "+palindromeRecursion("racecar"));
         endTime = System.nanoTime();
         System.out.println("Recursive Approach took "+(endTime-startTime)+" nanoseconds");
 
@@ -29,35 +29,30 @@ public class Palindrome{
     }
 
     public static boolean palindrome(String word){
-        if(word.length()%2!=0){
-            return false;
-        } else{
-            int lenTemp = word.length()-1;
-            boolean pal = true;
-            for(int i = 0; i < word.length()/2;i++){
-                if(word.charAt(i)==word.charAt(lenTemp)){
-                    lenTemp--;
-                } else{
-                    pal = false;
-                    break;
-                }
+        int lenTemp = word.length()-1;
+        boolean pal = true;
+        for(int i = 0; i < word.length()/2;i++){
+            if(word.charAt(i)==word.charAt(lenTemp)){
+                lenTemp--;
+            } else{
+                pal = false;
+                break;
             }
-            return pal;
         }
+        return pal;
     }
 
     public static boolean palindromeRecursion(String word){
-        if(word.length()==0){
+        if(word.length()<=1){
             return true;
+        } 
+        
+        if(word.charAt(0)==word.charAt(word.length()-1)){
+            return palindromeRecursion(word.substring(1,word.length()-1));
         } else{
-            if(word.length()%2!=0){
-                return false;
-            } else if(word.charAt(0)==word.charAt(word.length()-1)){
-                return palindromeRecursion(word.substring(1,word.length()-1));
-            } else{
-                return false;
-            }
+            return false;
         }
     }
+    
 
 }
