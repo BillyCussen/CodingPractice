e112910c9cce526e00cfef5796a92bd0b88cf5c9
BillyCussen <billycussen0@hotmail.com>
2020-12-08 23:04:17 +0000

    Finished DLL Implementation Revision in Python

--- Python/Data Structures & Algorithms Module - Python/Week10/DLLRevision/DLL.py
+++ Python/Data Structures & Algorithms Module - Python/Week10/DLLRevision/DLL.py
@@ -47,8 +47,63 @@ class DLL:
             newNode.successor = rightNode
             
 
+    def addBefore(self, element, target):
+        index = self.getIndex(target)
+        if self.isEmpty() or index==-1:
+            raise Exception("Can't add before "+str(target)+", DLL is Empty!")
+        else:
+            self.addByIndex(element,index)
+
+    def addAfter(self, element, target):
+        index = self.getIndex(target)+1
+        if self.isEmpty() or index==-1:
+            raise Exception("Can't add before "+str(target)+", DLL is Empty!")
+        else:
+            self.addByIndex(element,index)
+
+    def getIndex(self, element):
+        if self.isEmpty():
+            raise Exception("Can't get index of "+str(element)+", DLL is Empty!")
+        index = 0
+        current = self.first
+        while current != None:
+            if(current.element == element):
+                return index
+            index+=1
+            current = current.successor
+        return -1
+
     def isEmpty(self):
         return self.first == None
+
+    def removeByElement(self, target):
+        if(self.isEmpty()):
+            raise Exception("Unable to remove "+str(target)+", DLL is Empty!")
+        index = self.getIndex(target)
+        return self.removeByIndex(index)
+
+    def removeByIndex(self, index):
+        if(self.isEmpty()):
+            raise Exception("Unable to remove element at Index "+str(index)+", DLL is Empty!")
+        elif index < 0 or index >= self.size():
+            raise Exception("Unable to remove element at index "+str(index)+", Index is out of bounds")
+        elif index == 0:
+            element = self.first.element
+            self.first = self.first.successor
+            self.first.previous = None
+            if(self.isEmpty()):
+                self.last = None
+        else:
+            current = self.first
+            for i in range(index-1):
+                current = current.successor
+            element = current.successor.element
+            current.successor = current.successor.successor
+            if current.successor == None:
+                self.last = current
+            else:
+                current.successor.previous = current
+            return element
 
     def toString(self):
         if self.isEmpty():
--- Python/Data Structures & Algorithms Module - Python/Week10/DLLRevision/DLLApp.py
+++ Python/Data Structures & Algorithms Module - Python/Week10/DLLRevision/DLLApp.py
@@ -15,11 +15,35 @@ myDLL.add(6)
 print(myDLL.toString())
 print(myDLL.toStringBackwards())
 
-
 myDLL.addByIndex(20,0)
 myDLL.addByIndex(40,2)
 myDLL.addByIndex(60,4)
 myDLL.addByIndex(80,myDLL.size())
+
+print(myDLL.toString())
+print(myDLL.toStringBackwards())
+
+myDLL.addBefore(200,20)
+myDLL.addBefore(400,40)
+myDLL.addBefore(600,60)
+myDLL.addBefore(800,80)
+
+print(myDLL.toString())
+print(myDLL.toStringBackwards())
+
+
+myDLL.addAfter(2000,20)
+myDLL.addAfter(4000,40)
+myDLL.addAfter(6000,60)
+myDLL.addAfter(8000,80)
+
+print(myDLL.toString())
+print(myDLL.toStringBackwards())
+
+myDLL.removeByElement(2000)
+myDLL.removeByElement(4000)
+myDLL.removeByElement(6000)
+myDLL.removeByElement(8000)
 
 print(myDLL.toString())
 print(myDLL.toStringBackwards())
